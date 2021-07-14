f0577fac09c1aaaad9fa3168cf36ccdca3113368
Kabilar Gunalan <kabilar@vathes.com>
2021-07-14 10:42:23 -0500

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 from os import path
 
-pkg_name = 'element-data-loader'
+pkg_name = 'element_data_loader'
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), 'r') as f:
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='DataJoint',
-    author_email='info@vathes.com',
+    author_email='info@datajoint.com',
     license='MIT',
     url=f'https://github.com/datajoint/{pkg_name.replace("_", "-")}',
     keywords='neuroscience calcium-imaging science datajoint',
