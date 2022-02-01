765ccbee9871638702e13bafa7ade506d77f47f0
Tolga Dincer <tolgadincer@gmail.com>
2022-02-01 17:12:49 -0600

    Update image_converter.py

--- element_interface/image_converter.py
+++ element_interface/image_converter.py
@@ -5,8 +5,10 @@ import tifffile
 def nd2totif(nd2_file):
     """
     Convert Nikon .nd2 file to .tif file.
-
-    file: str
+    
+    Inputs
+    ------
+    nd2_file: str
         Path to the .nd2 file.
     """
     f = nd2.ND2File(nd2_file)
