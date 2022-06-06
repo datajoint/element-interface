768a008c3235b963d3a68c039cfa9cd517c107a4
Chris Broz <CBroz@datajoint.com>
2022-06-06 16:32:41 -0500

    Add my dev functions?

--- element_interface/utils.py
+++ element_interface/utils.py
@@ -3,6 +3,8 @@ import uuid
 import hashlib
 import csv
 
+import datajoint as dj
+from pymysql import  OperationalError
 
 def find_full_path(root_directories, relative_path):
     """
@@ -122,3 +124,38 @@ def recursive_search(key, dictionary):
             a = recursive_search(key, value)
             if a is not None: return a
     return None
+
+
+def list_schemas_prefix(prefix):
+    """ Returns list of schemas with a specific prefix """
+    return [s for s in dj.list_schemas() if s.startswith(prefix)]
+
+
+def clear_schemas(prefix=None, dry_run=True):
+    """
+    Cycles through schemas with specific prefix. If not dry_run, tries to drop.
+    Useful for dev, to save time figuring out the correct order in which to drop schemas
+
+    :param prefix: Optional. If not specified, uses dj.config prefix
+    :param dry_run: Optional. If True, returns list would attempt to drop
+    """
+    if not prefix:
+        try:
+            prefix = dj.config["custom"]["database.prefix"]
+        except KeyError:
+            raise NameError("Please specify a prefix with function or in dj.config")
+
+    schemas_with_prefix = list_schemas_prefix(prefix)
+
+    if dry_run:
+        print("\n".join(schemas_with_prefix))
+
+    else:
+        while schemas_with_prefix:
+            for i in schemas_with_prefix:
+                try:
+                    dj.schema(i).drop(force=True)
+                    schemas_with_prefix.remove(i)
+                    print(i)
+                except OperationalError:
+                    pass
